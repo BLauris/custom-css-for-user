92e0d079cece8f12ec72050de38a566d944db3ce
Lauris Bligzna <bligzna.lauris@gmail.com>
2015-10-27 11:14:13 +0200

    clan up

--- lib/generate_custom_style.rb
+++ lib/generate_custom_style.rb
@@ -1,13 +1,17 @@
 class GenerateCustomStyle
 
-  attr_reader :custom_style
+  attr_reader :custom_style, :body, :env, :filename, :file
 
   def initialize(style_id) 
     @custom_style = Style.find(style_id)
+    @filename = "#{custom_style.user_id}"
+    @file = File.new(File.join(tmp_themes_path, "#{filename}.scss"), "w")
+    @body = ERB.new(File.read(File.join(Rails.root, 'app', 'assets', 'stylesheets', '_template.scss.erb'))).result(binding)
+    @env = Rails.application.assets
   end
 
   def compile
-    file = File.new(File.join(tmp_themes_path, "#{filename}.scss"), "w")
+    create_temporary_file
 
     begin
       file.write generate_css
@@ -21,8 +25,8 @@ class GenerateCustomStyle
 
   private
 
-  def filename
-    "#{custom_style.user_id}"
+  def create_temporary_file
+    File.open(File.join(tmp_themes_path, "#{filename}.scss"), 'w') { |f| f.write(body) }
   end
     
   def tmp_themes_path
@@ -31,23 +35,8 @@ class GenerateCustomStyle
     @tmp_themes_path
   end
 
-  def create_asset
-    body = ERB.new(File.read(File.join(Rails.root, 'app', 'assets', 'stylesheets', '_template.scss.erb'))).result(binding)
-
-    File.delete(File.join(tmp_themes_path, "#{filename}.scss")) if File.exists?(File.join(tmp_themes_path, "#{filename}.scss"))
-    File.open(File.join(tmp_themes_path, "#{filename}.scss"), 'w') { |f| f.write(body) }
-    # binding.pry
-    # env = if Rails.application.assets.is_a?(Sprockets::Index)
-    #   Rails.application.instance_variable_get('@assets_manifest').instance_variable_get('@environment')
-    # else
-      env = Rails.application.assets
-    # end
-    # File.binread(file)
-    env.find_asset(filename)
-  end
-
   def generate_css
-    Sass::Engine.new(create_asset.source, {
+    Sass::Engine.new(env.find_asset(filename).source, {
       syntax: :scss,
       cache: false,
       read_cache: false,
